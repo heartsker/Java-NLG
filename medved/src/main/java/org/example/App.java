388ec0abcc27dc954ef02517336ba0c71bd130f8
heartsker <heartsker@gmail.com>
2023-06-18 11:58:22 +0300

    Updates main flow

--- medved/src/main/java/org/example/App.java
+++ medved/src/main/java/org/example/App.java
@@ -1,56 +1,107 @@
 package org.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class App {
+
+    private static Dataset dataset;
+    private static NGramManager nGramManager;
+    private final static Integer defaultMaxNGramDepth = 5;
+    private final static String defaultDatasetPath = "datasets/war_peace.txt";
+
+    private static Integer maxLength = 100;
+
     public static void main(String[] args) throws IOException {
-        Dataset dataset = new Dataset("datasets/war_peace.txt");
+
+        // Setup default dataset
+
+        String datasetPath = defaultDatasetPath;
+
+        if (args.length > 1) {
+            datasetPath = args[1];
+        }
+
+        setupDatasetWithDefaultDepth(datasetPath);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+
+            String command = reader.readLine();
+
+            // Setup new dataset
+            if (command.split(" ")[0].equals("setup")) {
+                datasetPath = command.split(" ")[1];
+
+                setupDatasetWithDefaultDepth(datasetPath);
+                continue;
+            }
+
+            // Setup new maxDepth
+            if (command.split(" ")[0].equals("depth")) {
+                int depth = Integer.parseInt(command.split(" ")[1]);
+
+                setupDataset(datasetPath, depth);
+                System.out.println("Set depth to " + depth);
+                continue;
+            }
+
+            // Setup max length
+            if (command.split(" ")[0].equals("length")) {
+                maxLength = Integer.parseInt(command.split(" ")[1]);
+                System.out.println("Set depth to " + maxLength);
+                continue;
+            }
+
+            // Exit
+            if (command.equals("exit") || command.equals("quit")) {
+                System.out.println("Quiting the application");
+                break;
+            }
+
+            // New prompt
+
+            handlePrompt(command);
+        }
+    }
+
+    private static void setupDatasetWithDefaultDepth(String filename) throws IOException {
+        setupDataset(filename, defaultMaxNGramDepth);
+    }
+
+    private static void setupDataset(String filename, Integer depth) throws IOException {
+        System.out.println("Setting up dataset from " + filename);
+
+        // Setup dataset
+
+        dataset = new Dataset(filename);
         dataset.setup();
-        System.out.println("============RAW==DATA============");
-        System.out.println(dataset.rawData);
+
         List<Integer> tokenized = dataset.tokenizer.tokenize(dataset.rawData);
-        System.out.println("============TOKENIZED============");
-        System.out.println(tokenized);
-        System.out.println("===========DETOKENIZED===========");
-        String detokenized = dataset.tokenizer.detokenize(tokenized);
-        System.out.println(detokenized);
 
-        System.out.println("============NGRAMS============");
+        // Setup NGramManager
+        nGramManager = new NGramManager(tokenized);
 
-//        String text = "The quick brown fox jumps over the lazy dog";
-//        List<Integer> tokenizedText = dataset.tokenizer.tokenize(text);
-//
-//        System.out.println("Text: " + text);
-//        System.out.println("Tokenized text: " + tokenizedText);
+        nGramManager.buildNGrams(depth);
 
-        NGramManager nGramManager = new NGramManager(tokenized);
 
-        nGramManager.buildNGrams(5);
-//        nGramManager.logNGrams();
+        System.out.println("Successfully set up dataset from " + filename);
+    }
 
-        String testText = "single armed";
-        List<Integer> tokens = dataset.tokenizer.tokenize(testText);
-
-        System.out.println("Test text: " + testText);
-        System.out.println("Tokenized test text: " + tokens);
-
-        List<Integer> nextTokens = nGramManager.getMostFrequentNextTokens(tokens);
-
-        List<String> nextWords = dataset.tokenizer.detokenizeSeparate(nextTokens);
-
-        System.out.println("For text [" + testText + "] the most frequent next tokens are: " + nextWords);
-
-        System.out.println("============GENERATING============");
+    private static void handlePrompt(String prompt) {
+        List<Integer> tokens = dataset.tokenizer.tokenize(prompt);
 
         Generator generator = new Generator(tokens, nGramManager);
 
-        List<Integer> generatedTokens = generator.generate(100);
-        List<String> generatedTextTokens = dataset.tokenizer.detokenizeSeparate(generatedTokens);
+        List<Integer> generatedTokens = generator.generate(maxLength);
         String generatedText = dataset.tokenizer.detokenize(generatedTokens);
 
-        System.out.println("Prompt: [" + testText + "]");
-        System.out.println("Generated text: \n" + generatedText);
+        System.out.println("==== Generated text from your prompt ====");
+        System.out.println(generatedText);
+        System.out.println("=========================================");
     }
 }
