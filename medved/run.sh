1b64e868c81d3a06b18f2faf7f161e97ded89460
Ilia Anashkin <iianashkin@edu.hse.ru>
2023-06-19 22:19:26 +0300

    FIX: cmd args

--- medved/run.sh
+++ medved/run.sh
@@ -1,9 +1,68 @@
-if [ "$1" = "all" ]; then
-    mvn clean package
-    docker build -t medved:local .
-    docker run --rm medved:local
+if [ "$1" = "build" ]; then
+    mvn clean package -Dmaven.test.skip
 fi
 
-if [ "$1" = "build" ]; then
-    mvn clean package
+if [ "$1" = "test" ]; then
+    mvn clean test
+fi
+
+if [ "$1" = "local" ]; then
+    shift
+    while [[ $# -gt 1 ]]; do
+        case $1 in
+            --dataset)
+            DATASET="$2"
+            shift
+            shift
+            ;;
+            --maxlength)
+            MAXLENGTH="$2"
+            shift
+            shift
+            ;;
+            --depth)
+            DEPTH="$2"
+            shift
+            shift
+            ;;
+        esac
+    done
+
+    echo "DATASET     = $DATASET"
+    echo "MAXLENGTH   = $MAXLENGTH"
+    echo "DEPTH       = $DEPTH"
+    echo "PROMPT      = $1"
+    mvn clean package -Dmaven.test.skip
+    java -Ddataset=$DATASET -Dmaxlength=$MAXLENGTH -Ddepth=$DEPTH -jar target/*.jar $1
+fi
+
+if [ "$1" = "docker" ]; then
+    shift
+    while [[ $# -gt 1 ]]; do
+        case $1 in
+            --dataset)
+            DATASET="$2"
+            shift
+            shift
+            ;;
+            --maxlength)
+            MAXLENGTH="$2"
+            shift
+            shift
+            ;;
+            --depth)
+            DEPTH="$2"
+            shift
+            shift
+            ;;
+        esac
+    done
+
+    echo "DATASET     = $DATASET"
+    echo "MAXLENGTH   = $MAXLENGTH"
+    echo "DEPTH       = $DEPTH"
+    echo "PROMPT      = $1"
+    mvn clean package -Dmaven.test.skip
+    docker build --progress=plain --build-arg "DATASET=$DATASET" --build-arg "MAXLENGTH=$MAXLENGTH" --build-arg "DEPTH=$DEPTH" -t medved:local .
+    docker run --rm medved:local $1
 fi
